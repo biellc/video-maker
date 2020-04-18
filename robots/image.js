96dd4a5bc49c8b3c7f0ab7e44e8954d33d9edcae
BuildTools <unconfigured@null.spigotmc.org>
2020-04-18 15:35:02 -0300

    add image download logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,13 +1,15 @@
+const imageDownloader = require('image-downloader')
 const { google } = require('googleapis')
 const customSearch = google.customsearch('v1')
 const state = require('./state')
 
 const googleSearchCredentials = require('../credentials/google.json')
 
-async function robot(){
+async function robot() {
     const content = state.load()
 
     await fetchImagesOfAllSentences(content)
+    await downloadAllImages(content)
 
     state.save(content)
 
@@ -34,6 +36,38 @@ async function robot(){
         })
 
         return imagesURL
+    }
+
+    async function downloadAllImages(content) {
+        content.downloadedImages = []
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            const images = content.sentences[sentenceIndex].images
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageURL = images[imageIndex]
+
+                try {
+                    if (content.downloadedImages.includes(imageURL)) {
+                        throw new Error('> Essa imagem já foi baixada')
+                    }
+
+                    await downloadAndSave(imageURL, `${sentenceIndex}-original.png`)
+                    content.downloadedImages.push(imageURL)
+                    console.log(`> [${sentenceIndex}] [${imageIndex}] Baixou imagem com sucesso: ${imageURL}`)
+                    break
+                } catch (error) {
+                    console.log(`> [${sentenceIndex}] [${imageIndex}] Erro ao baixar ${imageURL}: ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName) {
+        return imageDownloader.image({
+            url: url,
+            dest: `./content/${fileName}`
+        })
     }
 }
 
